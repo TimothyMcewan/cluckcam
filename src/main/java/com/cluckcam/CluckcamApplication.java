76f042717d3a03cf75e86180249f93f224977f31
Unknown <timothy_Mcewan@hotmail.co.nz>
2018-09-18 22:46:10 +1200

    Init Commit

---
+++ src/main/java/com/cluckcam/CluckcamApplication.java
@@ -0,0 +1,12 @@
+package com.cluckcam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CluckcamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CluckcamApplication.class, args);
+	}
+}
